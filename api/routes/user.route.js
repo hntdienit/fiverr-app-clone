165bc67394488f48df9c30cb3a195102bbabf59c
hntdienit <hntdienit@gmail.com>
2023-03-09 18:34:04 +0700

    api-set-up

---
+++ api/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { deleteUser } from "../controllers/user.controller.js";
+// import { deleteUser, getUser } from "../controllers/user.controller.js";
+// import { verifyToken } from "../middleware/jwt.js";
+
+const router = express.Router();
+
+// router.delete("/:id", verifyToken, deleteUser);
+// router.get("/:id", getUser);
+router.get("/test", deleteUser)
+
+export default router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,37 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB...");
+  } catch (error) {
+    console.log("Connected to MongoDB failure!");
+  }
+};
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.listen(8800, () => {
-    console.log("backend server is running....")
-})
+  connect();
+  console.log("Backend server is running...");
+});
